5a679d3c444aca832aaf95109fe5a960aa0737cc
Emilia Wróblewska <emilaw_23@o2.pl>
2018-05-20 15:48:18 +0200

    20-05-2018

--- zadania-domowe/zadanie-kurs-bitcoin-click/main.js
+++ zadania-domowe/zadanie-kurs-bitcoin-click/main.js
@@ -3,6 +3,7 @@
 
   var lastBuyValue = document.getElementById("kupno").innerHTML;
   console.log(lastBuyValue);
+
   var lastSellValue = document.getElementById("sprzedaz").innerHTML;
   console.log(lastSellValue);
 
@@ -10,61 +11,65 @@
   $(function () {
 
       $('#btn').on("click", function (e) {
-              $.getJSON('https://blockchain.info/pl/ticker', function (json) {
-                      console.log(json);
 
-                      var currentBuyValue = $("#kupno").prepend(json.THB.buy);
-                      showIconB();
+          $.getJSON('https://blockchain.info/pl/ticker', function (json) {
+              console.log(json);
 
-                      var currentSellValue = $("#sprzedaz").prepend(json.THB.sell);
-                      showIconS();
+              var currentBuyValue = $("#kupno").prepend(json.THB.buy);
+              showIconB();
 
-
-                      function showIconB() {
-                          if (lastBuyValue > currentBuyValue) {
-                              console.log("red");
-                              $("#stan-kupno .fa-arrow-down").addClass("down");
-                              $("#stan-kupno .fa-arrow-up").removeClass("up");
-                              $("#stan-kupno .fa-minus").removeClass("brak");
-
-                          }
-                          if (lastBuyValue < currentBuyValue) {
-                              console.log("green");
-                              $("#stan-kupno .fa-arrow-up").addClass("up");
-                              $("#stan-kupno .fa-arrow-down").removeClass("down");
-                              $("#stan-kupno .fa-minus").removeClass("brak");
-
-                          } else {
-                              console.log("blue");
-                              $("#stan-kupno .fa-minus").addClass("brak");
-                              $("#stan-kupno .fa-arrow-up").removeClass("up");
-                              $("#stan-kupno .fa-arrow-down").removeClass("down");
-                          };
-                      };
+              var currentSellValue = $("#sprzedaz").prepend(json.THB.sell);
+              showIconS();
 
 
-                      function showIconS() {
-                          if (lastSellValue > currentSellValue) {
-                              console.log("red");
-                              $("#stan-sprzedaz .fa-arrow-down").addClass("down");
-                              $("#stan-sprzedaz .fa-arrow-up").removeClass("up");
-                              $("#stan-sprzedaz .fa-minus").removeClass("brak");
-                          }
-                          if (lastSellValue < currentSellValue) {
-                              console.log("green");
-                              $("#stan-sprzedaz .fa-arrow-up").addClass("up");
-                              $("#stan-sprzedaz .fa-arrow-down").removeClass("down");
-                              $("#stan-sprzedaz .fa-minus").removeClass("brak");
-                          } else {
-                              console.log("blue");
-                              $("#stan-sprzedaz .fa-minus").addClass("brak");
-                              $("#stan-sprzedaz .fa-arrow-up").removeClass("up");
-                              $("#stan-sprzedaz .fa-arrow-down").removeClass("down");
-                          };
-                      };
+              function showIconB() {
+
+                  if (lastBuyValue > currentBuyValue) {
+                      console.log("red");
+                      $("#stan-kupno .fa-arrow-down").addClass("down");
+                      $("#stan-kupno .fa-arrow-up").removeClass("up");
+                      $("#stan-kupno .fa-minus").removeClass("brak");
+
+                  }
+                  if (lastBuyValue < currentBuyValue) {
+                      console.log("green");
+                      $("#stan-kupno .fa-arrow-up").addClass("up");
+                      $("#stan-kupno .fa-arrow-down").removeClass("down");
+                      $("#stan-kupno .fa-minus").removeClass("brak");
+
+                  } else {
+
+                      console.log("blue");
+                      $("#stan-kupno .fa-minus").addClass("brak");
+                      $("#stan-kupno .fa-arrow-up").removeClass("up");
+                      $("#stan-kupno .fa-arrow-down").removeClass("down");
                   };
+              };
 
-              });
+
+              function showIconS() {
+
+                  if (lastSellValue > currentSellValue) {
+                      console.log("red");
+                      $("#stan-sprzedaz .fa-arrow-down").addClass("down");
+                      $("#stan-sprzedaz .fa-arrow-up").removeClass("up");
+                      $("#stan-sprzedaz .fa-minus").removeClass("brak");
+                  }
+                  if (lastSellValue < currentSellValue) {
+                      console.log("green");
+                      $("#stan-sprzedaz .fa-arrow-up").addClass("up");
+                      $("#stan-sprzedaz .fa-arrow-down").removeClass("down");
+                      $("#stan-sprzedaz .fa-minus").removeClass("brak");
+
+                  } else {
+                      console.log("blue");
+                      $("#stan-sprzedaz .fa-minus").addClass("brak");
+                      $("#stan-sprzedaz .fa-arrow-up").removeClass("up");
+                      $("#stan-sprzedaz .fa-arrow-down").removeClass("down");
+                  };
+              };
+
+          });
       });
 
   });
